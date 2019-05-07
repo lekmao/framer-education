b5251eaa665be9066b9ee4f12a9189589c919863
Steve <steveruizok@gmail.com>
2019-05-07 19:15:52 +0100

    archives dither

--- projects/Archive/Dither.framerfx/code/Test.tsx
+++ projects/Archive/Dither.framerfx/code/Test.tsx
@@ -10,5 +10,39 @@ export function Test() {
         { scale: 1, rotate: 90 }
     )
 
-    return <Frame animate={twist} onTap={() => cycle()} size={"100%"} />
+    return (
+        <Frame
+            height={50}
+            width={80}
+            center
+            borderRadius={25}
+            onTap={flipSwitch}
+            variants={{
+                off: { background: "#BBBBBB" },
+                on: { background: "#0070DF" },
+            }}
+            initial={props.isOn ? "on" : "off"}
+            animate={state.isOn ? "on" : "off"}
+            transition={{
+                type: "tween",
+                duration: 0.2,
+            }}
+        >
+            <Frame
+                size={46}
+                top={2}
+                left={2}
+                borderRadius={"100%"}
+                background="#FFFFFF"
+                variants={{
+                    off: { x: 0 },
+                    on: { x: 30 },
+                }}
+                transition={{
+                    type: "tween",
+                    duration: 0.2,
+                }}
+            />
+        </Frame>
+    )
 }
