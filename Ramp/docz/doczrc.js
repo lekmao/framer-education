5a306aec9c365959a83451b566c304bf93ccffdb
Steve Ruiz <steveruizok@gmail.com>
2019-05-26 16:03:59 +0100

    Adds docz

---
+++ Ramp/docz/doczrc.js
@@ -0,0 +1,7 @@
+import doczPluginNetlify from 'docz-plugin-netlify'
+export default {
+	title: 'Framer Controller Docs',
+	description: 'Create Control state in Framer X with reusable controllers.',
+	themeConfig: {},
+	plugins: [doczPluginNetlify()],
+}
