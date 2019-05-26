9eadceb36f51a3467f3d437e5df31a7a2dcde84a
Steve Ruiz <steveruizok@gmail.com>
2019-05-26 21:04:10 +0100

    Archives some materials.

--- projects/Monokai.framerfx/code/canvas.tsx
+++
@@ -1,5 +0,0 @@
-// WARNING: this file is auto generated, any changes will be lost
-import { createDesignComponent, CanvasStore } from "framer"
-const canvas = CanvasStore.shared(); // CANVAS_DATA;
-export const Component = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,Title?:string,Left?:string,Center?:string,Right?:string}>(canvas, "id_Dg68Q38E3", {Title:"string",Left:"string",Center:"string",Right:"string"}, 375,375);
-export const DitherDemo = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,Title?:string,Left?:string,Center?:string,Right?:string}>(canvas, "id_eKXVTQkyy", {Title:"string",Left:"string",Center:"string",Right:"string"}, 375,375);
