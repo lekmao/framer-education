aec4cc96da83a82b9b90c507f821d86c340b0f80
Steve <steveruizok@gmail.com>
2019-07-02 11:38:48 +0100

    Updates Sockets

--- Projects/Sockets/Sockets.framerfx/code/App.tsx
+++ Projects/Sockets/Sockets.framerfx/code/App.tsx
@@ -3,51 +3,49 @@ import * as io from "socket.io-client"
 // @ts-ignore
 import { colors } from "@framer/steveruizok.education/code"
 
-// State
+// State --------------------------------------------------
 
 const appState = Data({
     connected: false,
     nightMode: false,
 })
 
-// Socket
+// Socket -------------------------------------------------
 
-const SOCKET_URL = "http://localhost:3000"
+// [1]
+const SERVER_URL = "http://localhost:3000"
+
+// [2]
 const CLIENT_ID = "FramerPrototype"
 
-const socket = io(SOCKET_URL, {
+// [3]
+const responses = {
+    FLIP_SWITCH: data => (appState.nightMode = data.value),
+}
+
+// [4]
+const sendAction = (type: string, data: { [key: string]: any } = {}) =>
+    socket.emit("action", { type, ...data })
+
+const socket = io(SERVER_URL, {
     reconnection: false,
     query: `id=${CLIENT_ID}`,
 })
 
-socket.on("connect", () => {
-    appState.connected = true
-})
+socket.on("connect", () => (appState.connected = true))
 
-socket.on("action", data => {
-    switch (data.type) {
-        case "FLIPSWITCH":
-            appState.nightMode = data.value
-            break
-        default:
-            return
-    }
-})
+socket.on("action", data => responses[data.type] && responses[data.type]())
 
-socket.on("disconnect", function() {
-    appState.connected = false
-})
+socket.on("disconnect", () => (appState.connected = false))
 
-// Overrides
+// Overrides ----------------------------------------------
 
 export const Switch: Override = () => {
     return {
         value: appState.nightMode,
         onValueChange: value => {
-            socket.emit("action", {
-                type: "FLIPSWITCH",
-                value,
-            })
+            // [5]
+            sendAction("FLIP_SWITCH", { value: value })
         },
     }
 }
@@ -64,3 +62,63 @@ export const Background: Override = () => {
         background: appState.nightMode ? "#2d2d2d" : "#fdfdfd",
     }
 }
+
+/* Notes __________________________________________________
+
+[1]
+The SERVER_URL variable points to the URL of the node
+server hosting the socket connection. If you've started
+the server on your own machine, then you are the "host"
+and may use "localhost:3000" for this variable.
+
+If testing this prototype with multiple machines, only one 
+machine needs to start the server. The other machines
+will need to use the host's local IP address (available
+from System Preferences > Network), together with the
+server's port (3000) in order to connect to the server.
+
+All machines will need to be on the same local network 
+(most likely a WIFI network). Each should have a
+unique CLIENT_ID to avoid conflicts. (See [2]).
+
+The result should look something like:
+
+    const SERVER_URL = "192.168.0.42:3000"
+    const CLIENT_ID = "FrankieKazarian"
+
+In this prototype, flipping the switch from a second machine
+would also flip the switch on all connected machines.
+
+- You'll need to update CLIENT_ID to a unique value for each
+  connected machine (see [2] below).
+
+[2]
+When connecting to a server, each machine will need to use
+a unique ID. This can be any string, but it must be different
+than the IDs used by other connected machines or else those
+other connections will be closed.
+
+[3]
+Whenever any connected client sends an actions, that action
+will be broadcast to all clients. Here we define how this
+prototype should respond to actions. In our case, we
+respond to a FLIP_SWITCH action by updating appState.nightMode.
+If we expected to receive a RESET_SWITCH action from some
+connected prototype, we could add a response like this:
+
+const responses = {
+    FLIP_SWITCH: data => (appState.nightMode = data.value),
+    RESET_SWITCH: () => appState.nightMode = false
+}
+
+[4]
+In this prototype, we can interact with the socket through
+"actions". See [5] for an example of creating an action.
+
+[5]
+We call sendAction with the action's type (ie FLIPSWITCH).
+We can also include an object with whatever data we want. 
+In our case, we're sending along either { value: true }
+or { value: false }, depending on whether we're flipped 
+the switch on or off.
+*/
--- Projects/Sockets/Sockets.framerfx/server/server.js
+++ Projects/Sockets/Sockets.framerfx/server/server.js
@@ -3,56 +3,41 @@ var app = require('express')()
 var server = http.createServer(app)
 var io = require('socket.io')(server)
 
-/*
-In this example, each client would subscribe with a unique
-id, stored using the query option.
+// 1. Socket Map
+// We'll keep track of connected sockets as "subscribers" and
+// "notify" each subscriber whenever new data arrives.
 
-For example, in our client...
-
-```
-const socket = io("http://localhost:3000", {
-    query: `id=clientA`,
-})
-```
-*/
-
-// Store a list of subscribers (connected sockets) that
-// We'll update each time any of the sockets emits an action
-
-const subscribers = {}
-const sockets = new Set()
+const subscribers = new Map()
 
 const subscribe = (id, socket) => {
-	if (subscribers[id]) {
+	if (subscribers.has(id)) {
 		unsubscribe(id)
 	}
 
-	subscribers[id] = socket
-	sockets.add(socket)
+	subscribers.set(id, socket)
 	console.log(`Connected to ${id}.`)
 }
 
 const unsubscribe = (id) => {
-	subscribers[id].disconnect()
-	sockets.delete(subscribers[id])
+	subscribers.delete(id)
 	console.log(`Disconnected from ${id}.`)
 }
 
-const updateSubscribers = (data) => {
-	sockets.forEach((socket) => socket.emit('action', data))
+const notifySubscribers = (data) => {
+	subscribers.forEach((socket) => socket.emit('action', data))
 }
 
-// When the server receives new connections, subscribe the new
-// connection and set event listeners to unsubscribe and respond
-// to actions.
+// 2. Server
+// When the server receives new socket connections, subscribe
+// the new socket and set event listeners.
 
 io.on('connection', (socket) => {
-	const { id } = socket.handshake.query
+	const { id = 'DefaultSocket' } = socket.handshake.query
 
 	subscribe(id, socket)
 
 	socket.on('action', (data) => {
-		updateSubscribers(data)
+		notifySubscribers(data)
 	})
 
 	socket.on('disconnect', () => {
@@ -60,17 +45,5 @@ io.on('connection', (socket) => {
 	})
 })
 
-// When the server receives a disconnection, double check that we've
-// removed the client from the set of subscribers
-
-io.on('disconnection', (socket) => {
-	const { id } = socket.handshake.query
-
-	if (subscribers[id]) {
-		unsubscribe(id)
-	}
-})
-
-// Start the server at http://localhost:3000
-
+// 3. Start the server
 server.listen(3000, () => console.log('Listening at http://localhost:3000'))
